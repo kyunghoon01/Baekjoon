c4e66b70aa7ebf01e3ee9f39aff7851280b230ae
kyunghoon01 <sports0124@naver.com>
2024-08-07 15:04:18 +0900

    [Bronze V] Title: A+B - 5, Time: 100 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,22 +1,27 @@
-import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-
-public class Main{
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String str;
-
-        while((str = br.readLine())!=null) {
-            if (str.equals("0 0"))
-                break;
-            int a = str.charAt(0) - 48;
-            int b = str.charAt(2) - 48;
-            sb.append(a+b).append('\n');
-        }
-
-        System.out.print(sb);
-    }
+import java.io.IOException;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			
+			String str = br.readLine();
+			
+			int A = str.charAt(0) - 48;
+			int B = str.charAt(2) - 48;
+            
+			if(A==0 && B==0) {
+				break;
+			}
+			sb.append((A+B)).append('\n');
+		}
+		System.out.println(sb);
+	}
 }
