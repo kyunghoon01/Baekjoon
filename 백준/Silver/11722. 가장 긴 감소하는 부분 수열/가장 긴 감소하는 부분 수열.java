0a654842323bb3c787ea2813dd23f49462c28e59
kyunghoon01 <sports0124@naver.com>
2024-08-08 16:19:42 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 120 ms, Memory: 15640 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // DP 배열 초기화
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        // 모든 이전 값들과 비교하여 dp 갱신
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (a[i] < a[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        // 가장 긴 감소하는 수열의 길이를 찾음
+        int maxLength = 0;
+        for (int i = 0; i < n; i++) {
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        
+        System.out.print(maxLength);
+    }
+}
